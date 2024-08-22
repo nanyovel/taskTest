77a53db7100aa75d8e65acc9f56b3ea8a59eaac1
nanyovel <jperezjimenez06@gmail.com>
2024-08-22 00:35:23 -0400

    probando funcionamiento inicial

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import authRouter from './routes/authRoutes.js'
 
 const app=express()
-
+// app.use(express(json))
+app.use(express.json());
 app.use('/api', authRouter)
 export default app
--- src/controller/authControllers.js
+++ src/controller/authControllers.js
@@ -1,3 +1,5 @@
 export const register=(req,res)=>{
+    const {username,password,email}=req.body
+    console.log(req.body)
     res.send('register')
 }
